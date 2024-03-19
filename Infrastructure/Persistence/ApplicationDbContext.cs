29e7610e01aa73cf3c0dd6069305fda73cbfe7fb
JuanSDev <juansdev72@gmail.com>
2024-03-18 21:57:01 -0500

    feat: Capa de Infraestructura

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Application.Data;
+using Domain.Customers;
+using Domain.Primitives;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext: DbContext, IApplicationDbContext, IUnitOfWork
+{
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
+    {
+        _publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    public async Task<int> SaveChangeAsync(CancellationToken cancellationToken = default)
+    {
+        var domainEvents = ChangeTracker.Entries<AggregateRoot>()
+            .Select(e => e.Entity)
+            .Where(e => e.GetDomainEvents().Any())
+            .SelectMany(e => e.GetDomainEvents());
+        var result = await base.SaveChangesAsync(cancellationToken);
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent, cancellationToken);
+        }
+
+        return result;
+    }
+}
