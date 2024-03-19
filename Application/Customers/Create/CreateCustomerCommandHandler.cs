9144c592a06c39bae41f560344e112504468cb03
JuanSDev <juansdev72@gmail.com>
2024-03-18 21:42:01 -0500

    feat: Capa de Aplicación

---
+++ Application/Customers/Create/CreateCustomerCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Customers;
+using Domain.Primitives;
+using Domain.ValueObjects;
+using MediatR;
+
+namespace Application.Customers.Create;
+
+public sealed class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, Unit>
+{
+    private readonly ICustomerRepository _customerRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateCustomerCommandHandler(ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
+    {
+        _customerRepository = customerRepository ?? throw new ArgumentException(nameof(customerRepository));
+        _unitOfWork = unitOfWork ?? throw new ArgumentException(nameof(unitOfWork));
+    }
+
+    public async Task<Unit> Handle(CreateCustomerCommand command, CancellationToken cancellationToken)
+    {
+        if (PhoneNumber.Create(command.PhoneNumber) is not PhoneNumber phoneNumber)
+        {
+            throw new ArgumentException(nameof(phoneNumber));
+        }
+        
+        if(Address.Create(command.Country, command.Line1, command.Line2, command.City, command.State, command.ZipCode) is not Address address)
+        {
+            throw new ArgumentException(nameof(address));
+        }
+        
+        var customer = new Customer(
+            new CustomerId(Guid.NewGuid()),
+            command.Name,
+            command.LastName,
+            command.Email,
+            phoneNumber,
+            address,
+            true
+        );
+        await _customerRepository.Add(customer);
+        await _unitOfWork.SaveChangeAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Customer> Customers { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
